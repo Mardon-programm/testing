027c50bb874c2c1b4537daf396678b5698a1f11e
Mardon-programm <creatermari@gmail.com>
2025-05-06 23:50:48 +0600

    to commit

---
+++ core/settings/base.py
@@ -0,0 +1,170 @@
+import os
+from pathlib import Path
+from datetime import timedelta
+from django.utils.translation import gettext_lazy as _
+from core.settings.jazzmin import JAZZMIN_SETTINGS
+
+from dotenv import load_dotenv
+
+load_dotenv()
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+#SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-6hsivhlmq8-fv4$0hy9&_^pcjsp)o*f1poic6eegmzhb@#1ht5'
+
+if DEBUG:
+    try:
+        from .development import *
+    except ImportError:
+        raise ImportError("Файл development.py не найден")
+else:
+    try:
+        from .production import *
+    except ImportError:
+        raise ImportError("Файл production.py не найден")
+
+THEME_APPS = [
+    "jazzmin",
+]
+
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+MY_APPS = [
+    # приложения
+
+]
+
+
+LIBRARY_APPS = [
+    'modeltranslation',
+    "rest_framework",
+    "corsheaders",
+    'drf_yasg',
+    'ckeditor',
+    'django_ckeditor_5',
+]
+
+LANGUAGES = [
+    ('ru', _('Russian')),
+    ('ky', _('Kyrgyz')),
+    ('en', _('English')),
+]
+
+
+
+TRANSLATABLE_MODEL_MODULES = ['app.base.models']
+
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+    os.path.join(BASE_DIR, 'locale'),
+]
+
+INSTALLED_APPS = [
+    *THEME_APPS,
+    *DJANGO_APPS,
+    *MY_APPS,
+    *LIBRARY_APPS,
+]
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
+CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'  # URL to jQuery
+CKEDITOR_IMAGE_BACKEND = "pillow"  # Путь к пакету Pillow для обработки изображений
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',  # Вы можете настроить свою собственную панель инструментов CKEditor
+        'height': 300,
+        'width': 800,
+    },
+}
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+]
+
+MODELTRANSLATION_DEFAULT_LANGUAGE = 'ky'
+
+ROOT_URLCONF = "core.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+
+WSGI_APPLICATION = "core.wsgi.application"
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+LANGUAGE_CODE = "ru"
+
+TIME_ZONE = "Asia/Bishkek"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+JAZZMIN_SETTINGS=JAZZMIN_SETTINGS
+AUTH_USER_MODEL = "auth.User"
+
+CKEDITOR_5_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+        'height': 300,
+        'width': '100%',
+        'config': {
+            'language': 'en',
+        },
+    },
+}
---
+++ core/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from core.yasg import urlpatterns_yasg
+
+
+# Основные маршруты (не зависят от мультиязычности)
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include(urlpatterns_yasg)),  # ✅ Swagger (yasg)
+]
+
+# Маршруты, зависящие от языка (i18n)
+urlpatterns += i18n_patterns(
+    #path('api/v1/ваше приложение/', include('app.ваше приложение.urls')),
+)
+
+# Раздача статических и медиа-файлов
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
